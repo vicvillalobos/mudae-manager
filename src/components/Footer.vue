c17f228f06b37d9d2a2bd8d2d2cbcadac7b30477
Victor Villalobos <vvillalobos@Air-de-Victor.lan>
2022-06-11 02:40:25 -0500

    Might as well edit the footer again, since I already made the tag

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
             <div class="col-md-12">
                 <div class="footer-text">
                     <strong>Danpen</strong> - Helper web application for the Discord bot <strong>Mudae</strong>.<br/>
-                    <small>Version 0.4.0 - This project is in no way affiliated to the bot. </small>
+                    <small>Version 0.5.1 - This project is in no way affiliated to the bot. </small>
                 </div>
             </div>
         </div>
