e387ab8f87d9ed274af0fc6fbdbad4310fa87497
Victor Villalobos <vvillalobos@we-techs.com>
2022-06-16 00:29:27 -0400

    Added base to vite config & created new fixed build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mudae-manager/',
   resolve: { dedupe: ["vue"] },
   plugins: [vue()],
   server: {
